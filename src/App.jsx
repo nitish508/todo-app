86aae3c2ad001d929efe3421c84cb90da0161954
Nitish <nitishkumaryaduvanshi1@gmail.com>
2024-03-04 21:52:43 +0530

    localStorage feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { useCallback, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./header/Header.jsx";
 import Body from "./body/Body.jsx";
 
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const storedTodoLists = JSON.parse(localStorage.getItem("todoLists"));
+    if (storedTodoLists) {
+      setTodoLists(storedTodoLists);
+    }
+  }, []);
   console.log("App render");
   const [todoLists, setTodoLists] = useState([]);
-  // Add todos
   const handleClick = (value) => {
     const updatedValue = {
       id: Math.random() * 10,
       text: value,
     };
-    setTodoLists((prevState) => [...prevState, updatedValue]);
+    const updatedLists = [...todoLists, updatedValue];
+    setTodoLists(updatedLists);
+    localStorage.setItem("todoLists", JSON.stringify(updatedLists));
   };
 
   const handleDelete = (id) => {
     setTodoLists((prevState) => {
-      return prevState.filter((item) => item.id !== id);
+      const updatedLists = prevState.filter((item) => item.id !== id);
+      localStorage.setItem("todoLists", JSON.stringify(updatedLists));
+      return updatedLists;
     });
   };
 
